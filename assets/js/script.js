75ecc89eb979b63d5a94e7568a3a26d156b90757
Willames da S. Barbosa <104446605+WillamesBarbosa@users.noreply.github.com>
2022-07-19 17:47:38 -0300

    Primeiro commit
    Primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function abrirModal(){
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'flex';
+}
+
+function fecharModal(){
+    let modal = document.querySelector('.modal');
+    modal.style.display = 'none';
+}
